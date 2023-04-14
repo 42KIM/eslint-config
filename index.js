be5a219283e660d7b2b5621479cf10fc095f5d22
42KIM <ksahoo524@gmail.com>
2023-04-14 19:21:56 +0900

    fix Unexpected top-level property "files" error by adding overrides prop

--- index.js
+++ index.js
@@ -4,45 +4,49 @@ module.exports = {
     project: true
   },
   plugins: ['@typescript-eslint'],
-  files: ['*.ts', '*.tsx'],
-  rules: {
-    '@typescript-eslint/consistent-type-imports': [
-      'error',
-      { 
-        'prefer': 'type-imports' 
-      }
-    ],
-    '@typescript-eslint/member-delimiter-style': [
-      'error',
-      {
-        'multiline': {
-          'delimiter': 'comma'
-        },
-        'singleline': {
-          'delimiter': 'comma'
-        },
-        'overrides': {
-          'interface': {
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      rules: {
+        '@typescript-eslint/consistent-type-imports': [
+          'error',
+          { 
+            'prefer': 'type-imports' 
+          }
+        ],
+        '@typescript-eslint/member-delimiter-style': [
+          'error',
+          {
             'multiline': {
-              'delimiter': 'semi'
+              'delimiter': 'comma'
+            },
+            'singleline': {
+              'delimiter': 'comma'
+            },
+            'overrides': {
+              'interface': {
+                'multiline': {
+                  'delimiter': 'semi'
+                }
+              }
             }
           }
-        }
+        ],
+        '@typescript-eslint/naming-convention': [
+          'error',
+          {
+            'selector': 'typeLike',
+            'format': ['PascalCase']
+          }
+        ],
+        '@typescript-eslint/no-duplicate-type-constituents': 'error',
+        '@typescript-eslint/no-confusing-void-expression': 'error',
+        '@typescript-eslint/no-empty-interface': 'error',    
+        '@typescript-eslint/comma-dangle': ['error', 'always-multiline'],
+        '@typescript-eslint/comma-spacing': 'error',
+        '@typescript-eslint/object-curly-spacing': ['error', 'always'],
+        '@typescript-eslint/semi': 'error'
       }
-    ],
-    '@typescript-eslint/naming-convention': [
-      'error',
-      {
-        'selector': 'typeLike',
-        'format': ['PascalCase']
-      }
-    ],
-    '@typescript-eslint/no-duplicate-type-constituents': 'error',
-    '@typescript-eslint/no-confusing-void-expression': 'error',
-    '@typescript-eslint/no-empty-interface': 'error',    
-    '@typescript-eslint/comma-dangle': ['error', 'always-multiline'],
-    '@typescript-eslint/comma-spacing': 'error',
-    '@typescript-eslint/object-curly-spacing': ['error', 'always'],
-    '@typescript-eslint/semi': 'error'
-  }
+    }
+  ]
 }
